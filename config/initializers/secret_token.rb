7a91babb77c458370fb24a537d4ccf5b16f458b4
TerryBeyak <terry@bluebirdinteractive.net>
2012-10-18 13:10:51 -0700

    Initialize.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Factcheck::Application.config.secret_token = 'd87b81d7efebbba352f0a6617cc7f9cb5f063ffbd65f928ab03d00d8dd426c7a0c5f95a5f27e7e60b1c38077579d79ab8359264beafa5caeabe81e62abd8993f'
